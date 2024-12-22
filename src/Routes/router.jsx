5376f2bf638f3da411e342d31515a0abf666fc64
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-22 12:02:40 +0600

    all route setup

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home";
 import Register from "../Pages/Register";
 import Login from "../Pages/Login";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,51 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path: "/all-foods",
+        element: <h2>All Foods</h2>,
+      },
+      {
+        path: "/gallery",
+        element: <h2>gallery</h2>,
+      },
+      {
+        path: "/food/:id",
+        element: <h2>single food page</h2>,
+      },
+      {
+        path: "/my-foods",
+        element: (
+          <PrivateRoute>
+            <h2>my-foods</h2>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/add-food",
+        element: (
+          <PrivateRoute>
+            <h2>add-food</h2>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/my-orders",
+        element: (
+          <PrivateRoute>
+            <h2>my-orders</h2>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/purchase/:id",
+        element: (
+          <PrivateRoute>
+            <h2>purchase/:id</h2>
+          </PrivateRoute>
+        ),
+      },
+
       {
         path: "/login",
         element: <Login></Login>,
